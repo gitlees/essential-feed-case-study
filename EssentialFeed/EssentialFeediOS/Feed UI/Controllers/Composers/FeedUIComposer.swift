2df5beb09f32866ba8b08c118d9b8a2c161774ad
Stanislav Lee <glees769@gmail.com>
2021-08-22 15:01:53 +0600

    Feed MVC components migrated to MVVM

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/Composers/FeedUIComposer.swift
@@ -7,6 +7,7 @@
 //
 
 import EssentialFeed
+import UIKit
 
 public final class FeedUIComposer {
     private init() {}
@@ -23,7 +24,7 @@ public final class FeedUIComposer {
     private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> (([FeedImage]) -> Void) {
         return { [weak controller] feed in
             controller?.tableModel = feed.map {
-                FeedImageCellController(model: $0, imageLoader: loader)
+                FeedImageCellController(viewModel: FeedImageViewModel(model: $0, imageLoader: loader, imageTransformer: UIImage.init))
             }
         }
     }
